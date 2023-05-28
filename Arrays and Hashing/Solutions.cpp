2f4048f845ba7453231e4e7b99a660d622259519
srxg <130320493+srxg@users.noreply.github.com>
2023-05-28 17:28:42 +0100

    longest consecutive sequence

--- Arrays and Hashing/Solutions.cpp
+++ Arrays and Hashing/Solutions.cpp
@@ -148,6 +148,28 @@ vector<int> productExceptSelf(vector<int>& nums) {
     return answer;
 }
 
+int longestConsecutive(vector<int>& nums) {
+    unordered_set<int> numAsSet(nums.begin(), nums.end());
+    int longestStreak = 0;
+
+    for (auto& num : numAsSet) {
+        if (!numAsSet.count(num - 1)) {
+            int currentNum = num;
+            int currentStreak = 1;
+
+            while (numAsSet.count(currentNum + 1)) {
+                currentNum++;
+                currentStreak++;
+            }
+            
+            longestStreak = max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+}
+
+
 int main() {
     std::vector<int> ex_nums = {1,2,2,3};
     std::cout << containsDuplicate(ex_nums) << std::endl;
